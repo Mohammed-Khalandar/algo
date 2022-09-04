1265f241a663e5c6df661553e57d2d30cfa8327f
Mohammed-Khalandar <89445636+Mohammed-Khalandar@users.noreply.github.com>
2022-09-04 14:09:46 +0530

    Update two_sum.py

--- algo/two_sum.py
+++ algo/two_sum.py
@@ -1,11 +1,23 @@
-def two_sum(array,targetSum):
-    for i in array:
-        diff = targetSum -i
-        if diff != i and diff in array:
-            return [diff,i]
-    return []
-
+def two_sum(nums,target):
+#     for i in array:
+#         diff = targetSum -i
+#         if diff != i and diff in array:
+#             return [diff,i]
+#     return []
+# Solution 1
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        nums[i] = None
+        if  diff in nums:
+            return ([i,nums.index(diff)])
+#Solution 2
+    for i, value in enumerate(nums):
+        remaining = target - nums[i]
+        nums[i] = None
+        if remaining in nums:
+            return [i, nums.index(remaining)]
+        
 if __name__ == '__main__':
-    array = [3, 5, -4, 8, 11, 1, -1, 6]
-    targetSum = 10
-    print(two_sum(array,targetSum))
+    nums = [3, 5, -4, 8, 11, 1, -1, 6]
+    target = 10
+    print(two_sum(nums,target))
